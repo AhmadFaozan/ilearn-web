d5cf7313bc8d05832a9b517e635e0698038569b6
AhmadFaozan <afaozan47@gmail.com>
2023-07-25 13:42:18 +0700

    ilearn

---
+++ index.js
@@ -0,0 +1,10 @@
+// Awal bagian Ahmad Faozan
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function () {
+    nav.classList.toggle('slide');
+});
+// Akhir bagian Ahmad Faozan
+
+// Silahkan tambahkan javascript teman-teman disini jika diperlukan 
